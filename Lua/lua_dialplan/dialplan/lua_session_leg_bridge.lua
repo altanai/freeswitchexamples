354c6c8c520ce001f1aa737778aae5baa4879c1b
altanai <tara181989@gmail.com>
2019-09-25 15:07:54 +0530

    third party call control over Lua

--- Lua/lua_dialplan/dialplan/lua_session_leg_bridge.lua
+++ Lua/lua_dialplan/dialplan/lua_session_leg_bridge.lua
@@ -1,48 +1,44 @@
+freeswitch.consoleLog("WARNING","-------- Before first call")
 
-
-freeswitch.consoleLog("WARNING","Before first call")
-first_session = freeswitch.Session("user/1011")
+first_session = freeswitch.Session("user/altanai")
 if (first_session:ready()) then  
-freeswitch.consoleLog("WARNING","first leg answeredn")
-second_session = freeswitch.Session("user/1010")  
-if (second_session:ready()) then
-freeswitch.consoleLog("WARNING","second leg answeredn")  
-freeswitch.bridge(first_session, second_session)
-freeswitch.consoleLog("WARNING","After bridgen")  
-else
-freeswitch.consoleLog("WARNING","second leg failedn")  
-end
+	freeswitch.consoleLog("WARNING","--------- first leg answered")
+
+	second_session = freeswitch.Session("user/bob")  
+	if (second_session:ready()) then
+		freeswitch.consoleLog("WARNING","------- second leg answered")  
+
+		freeswitch.bridge(first_session, second_session)
+		freeswitch.consoleLog("WARNING","--------- After bridging")  
+	else
+		freeswitch.consoleLog("WARNING","second leg failed")  
+	end
 else  
-freeswitch.consoleLog("WARNING","first leg failedn")
+	freeswitch.consoleLog("WARNING","first leg failed")
 end
 
-
-
-
+-- Script for sipp to start the first call to 5001 , after which FS dials out altanaia nd bob 
+-- sipp -sn uac -s 5001 x.x.x.x:5060 -m 1 -d 20000 -mp 6001 -mi 127.0.0.1 
 
 --[[
-Output
+Output :
+Dialplan: sofia/internal/bob@x.x.x.x Action lua(/etc/freeswitch/dialplan/lua_session_leg_bridge.lua) 
+EXECUTE sofia/internal/bob@x.x.x.x lua(/etc/freeswitch/dialplan/lua_session_leg_bridge.lua)
 
-2019-09-25 07:44:33.503863 [NOTICE] switch_channel.c:1104 New Channel sofia/internal/altanai@106.51.78.22:50037 [310277e3-4e42-41d0-8906-dba93e35c5d6]
+...Incoming call
+[NOTICE] switch_channel.c:1104 New Channel sofia/internal/altanai@x.x.x.x:7494 [a096c009-3045-4f03-a416-b652397a8b29]
+EXECUTE sofia/internal/sipp@fx.x.x.x:5060 lua(/etc/freeswitch/dialplan/lua_session_leg_bridge.lua)
 
+[WARNING] switch_cpp.cpp:1376 -------- Before first call
+.. Leg 1 
+[NOTICE] switch_channel.c:1104 New Channel sofia/internal/altanai@x.x.x.x:5284 [ed7a825a-3e08-4372-82ca-428b1778a409]
+[NOTICE] sofia.c:8427 Channel [sofia/internal/altanai@x.x.x.x:5284] has been answered
+[WARNING] switch_cpp.cpp:1376 --------- first leg answered
+[DEBUG] switch_ivr_originate.c:2159 Parsing global variables
+.. Leg 2 
+[NOTICE] switch_channel.c:1104 New Channel sofia/internal/bob@x.x.x.x:35733 [6f40d42a-fbca-4af3-a772-36c344b7d75c]
+[NOTICE] sofia.c:8427 Channel [sofia/internal/bob@x.x.x.x:35733] has been answered
+[WARNING] switch_cpp.cpp:1376 ------- second leg answered
 
-   INVITE sip:altanai@106.51.78.22:50037;rinstance=c14e4a9ba6020f9b SIP/2.0
-   Via: SIP/2.0/UDP 10.130.74.15;rport;branch=z9hG4bK31Z086ce8UtXK
-   Max-Forwards: 70
-   From: "" <sip:0000000000@10.130.74.15>;tag=3SvHQr98DK0vN
-   To: <sip:altanai@106.51.78.22:50037;rinstance=c14e4a9ba6020f9b>
-   Call-ID: 281334aa-5a0b-1238-d29d-02a933b32da0
-   CSeq: 10144944 INVITE
-   Contact: <sip:mod_sofia@10.130.74.15:5060>
-   User-Agent: FreeSWITCH-mod_sofia/1.9.0-742-8f1b7e0~64bit
-   Allow: INVITE, ACK, BYE, CANCEL, OPTIONS, MESSAGE, INFO, UPDATE, REGISTER, REFER, NOTIFY, PUBLISH, SUBSCRIBE
-   Supported: timer, path, replaces
-   Allow-Events: talk, hold, conference, presence, as-feature-event, dialog, line-seize, call-info, sla, include-session-description, presence.winfo, message-summary, refer
-   Content-Type: application/sdp
-   Content-Disposition: session
-   Content-Length: 1210
-   X-FS-Support: update_display,send_info
-   Remote-Party-ID: <sip:0000000000@10.130.74.15>;party=calling;screen=yes;privacy=off
-
-
-   ]]
+[DEBUG] switch_ivr_bridge.c:1639 (sofia/internal/bob@x.x.x.x:35733) State Change CS_SOFT_EXECUTE -> CS_CONSUME_MEDIA
+ ]]
